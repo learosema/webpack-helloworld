e153a596da23b4b66b9bc7f309870513f79193f3
Lutz Rosema <terabaud@gmail.com>
2016-08-16 14:01:46 +0200

    added more example code

--- src/greeter.js
+++ src/greeter.js
@@ -1,5 +1,15 @@
 export default class Greeter {
+
+    /**
+     * @param who who to greet (default: World)
+     * @param container the host element
+     */
+    constructor(who, container) {
+        this.container = container || document.body;
+        this.who = who || "World";
+    }
+
     sayHello() {
-        document.write("Hallo, World!");
+        this.container.innerHTML= `<p>Hello, ${this.who}! ☺</p>`;
     }
 }
